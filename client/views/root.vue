d5d9cdebb6326e0cb08baee058d4d08cd294918c
ruosen.chai <chairuosen@gmail.com>
2016-06-17 18:57:48 +0800

    import 3rd editor

--- client/views/root.vue
+++ client/views/root.vue
@@ -1,19 +1,70 @@
 <style lang="less" rel="stylesheet/less">
-
+    *{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+    }
+    html,body{
+        height:100%;
+    }
+    .left,.right{
+        position:absolute;
+        top:0;
+        width:50%;
+        height:100%;
+    }
+    .left {
+        left:0;
+        .editor{
+            width:100%;
+            height:100%;
+        }
+    }
+    .right{
+        right:0;
+    }
 </style>
 <template>
-    hello
+    <div class="left">
+        <div id="editor" class="editor">111</div>
+    </div>
+    <div class="right">
+        <iframe v-el:iframe frameborder="0"></iframe>
+    </div>
 </template>
 
 <script>
+    var ace = require('brace');
+    require('brace/mode/javascript');
+    require('brace/theme/monokai');
+
+
+
     module.exports = {
         data: function () {
-            return {}
+            return {
+                source:{
+                    html:""
+                }
+            }
         },
-        methods: {},
+        methods: {
+            preview:function (html) {
+                var iframe = this.$els.iframe;
+                var _window = iframe.contentWindow;
+                _window.document.documentElement.innerHTML = html;
+            }
+        },
+        watch:{
+            'source.html':function (val) {
+                this.preview(val);
+            }
+        },
         components: {},
         ready: function () {
-
+            var editor = ace.edit('editor');
+            editor.getSession().setMode('ace/mode/javascript');
+            editor.setTheme('ace/theme/monokai');
         }
     }
 </script>
