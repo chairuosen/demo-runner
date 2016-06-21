40a228ba0f5000637ee4a5242fbb514342bc8eae
ruosen.chai <chairuosen@gmail.com>
2016-06-21 10:20:38 +0800

    add api rate limit

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var RateLimit = require('express-rate-limit');
 var app = express();
 var bodyParser = require('body-parser');
 var argv = require('yargs').argv;
 var port = argv.port || 5000;
+
+app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+
+var minutes = 60*1000;
+var limiter = new RateLimit({
+    windowMs: 1*minutes, // s
+    max: 30, // limit each IP to 100 requests per windowMs
+    delayMs: 0 // disable delaying - full speed until the max limit is reached
+});
+
+app.use('/api/',limiter);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
